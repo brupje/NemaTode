25e1c3d7e7c5942e6f504d6eb837253987d36581
brupje <info@bnhmeijer.nl>
2026-05-02 08:21:55 +0200

    add comment

--- src/GPSService.cpp
+++ src/GPSService.cpp
@@ -189,6 +189,7 @@ void GPSService::read_GxGGA(const NMEASentence& nmea){
 		// FIX QUALITY
 		bool lockupdate = false;
 		this->fix.quality = (uint8_t)parseInt(nmea.parameters[5]);
+		// If we have a good quality, assume we have a fix
 		if (this->fix.quality == 4 || this->fix.quality == 5) {
 			this->fix.type = 3; 
 			lockupdate = this->fix.setlock(true);
